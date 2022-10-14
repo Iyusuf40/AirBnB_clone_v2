7c1fcbb07efc9a7ca6e0e82c53782fcca012ee20
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-10-14 14:30:56 +0000

    add is_number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" A simple flask app """
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """root route"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """hbnb route"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """returns C <text>"""
+    if (type(text) is str):
+        text = text.replace('_', ' ')
+        return "C " + text
+    from flask import Abort
+    Abort(404)
+
+
+@app.route('/python/', redirect_to='python/is_cool', strict_slashes=False)
+def python_re_route(text="is cool"):
+    """returns Python <text>"""
+    pass
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text="is cool"):
+    """returns Python <text>"""
+    text = text.replace('_', ' ')
+    return "Python " + text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    """checks if n is number"""
+    return f"{n} is number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
