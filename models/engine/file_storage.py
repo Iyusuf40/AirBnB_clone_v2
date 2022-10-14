2263e4ceeef89f5ba2ca3f868512fad2b6c1dff0
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-10-14 17:22:42 +0000

    add close methods tp close sessions

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -63,3 +63,7 @@ class FileStorage:
             if key in self.all():
                 del self.all()[key]
             self.save()
+
+    def close(self):
+        """closes a session"""
+        self.reload()
