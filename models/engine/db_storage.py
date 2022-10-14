08d39690759d179751fc4de0795aa2b3c94b2e6a
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-10-14 18:54:20 +0000

    remove global

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -83,7 +83,7 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
-        global Session
+        # global Session
         Session = scoped_session(session_factory)
         self.__session = Session()
 
