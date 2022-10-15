31309ade22fc9d92a9165c1c49f25271e6a049af
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-10-15 06:42:15 +0000

    test

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -26,6 +26,5 @@ def static_list():
         dct[key] = states[item].name
     return render_template('7-states_list.html', states=dct)
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
